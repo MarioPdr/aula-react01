a11a0378cc56efe0b87170fb02c59ed0d90a201d
MarioPdr <mario.coctop@gmail.com>
2024-11-05 22:36:03 -0300

    Aula 9 - Material UI Componentes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } 
-from 'react-router-dom';
-import Home from './components/Aula 8/Home';
-import Users from './components/Aula 8/Users';
-import UserDetail from './components/Aula 8/UserDetail';
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/users">Users</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<Users />}>
-            {/* Rota Aninhada - usando parametros na URL */}
-            <Route path=":userId" element={<UserDetail />} />
-          </Route>
-          {/* Redirecionamento em caso de rota vazia ou não
-          existente */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+import FormularioContato from './components/Aula 9/FormularioContato';
+const App = () => (
+  <>
+    <FormularioContato />
+  </>
+);
 
 export default App;
--- my-app/src/components/Aula 8/Users.js
+++ my-app/src/components/Aula 8/Users.js
@@ -1,4 +1,3 @@
-// components/Users.js
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 function Users() {
